13337f3ebd885fb5b871da66a609034227ee1d0b
ashly <ashlyannarackal@gmail.com>
2023-06-19 20:46:18 +0530

    changes in angle calculation and timer added

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
->>>>>>> fruit
-=======
 # fruitcollection
 
 THe game control is based on shoulder movement. The movement is defined by angle by elbow,hip and shoulder coordinates.
->>>>>>> 9c84821e2e7500c2b0549f3b034bfffc96ae6643
+
--- lib/vision_detector_views/pose_detector_view.dart
+++ lib/vision_detector_views/pose_detector_view.dart
@@ -1,26 +1,22 @@
 import 'dart:math';
-
+import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
-import 'package:vector_math/vector_math.dart';
 import 'package:fruitgame/game.dart';
-
 import 'camera_view.dart';
 import 'painters/pose_detector_painter.dart';
 
 class PoseDetectorView extends StatefulWidget {
-  PoseDetectorView({Key? key});
+  const PoseDetectorView({Key? key}) : super(key: key);
 
   @override
   State<PoseDetectorView> createState() => _PoseDetectorViewState();
 }
 
 class _PoseDetectorViewState extends State<PoseDetectorView> {
-  //int intial = 0;
   final PoseDetector _poseDetector = PoseDetector(
-      options: PoseDetectorOptions(
-          model: PoseDetectionModel.base, mode: PoseDetectionMode.stream));
+      options: PoseDetectorOptions(model: PoseDetectionModel.base));
 
   bool _canProcess = true;
   bool _isBusy = false;
@@ -55,7 +51,7 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       _text = '';
     });
 
-    final poses = await _poseDetector.processImage(inputImage);
+    var poses = await _poseDetector.processImage(inputImage);
 
     for (Pose pose in poses) {
       PoseLandmark rightHip = pose.landmarks[PoseLandmarkType.rightHip]!;
@@ -68,9 +64,11 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       PoseLandmark leftShoulder =
           pose.landmarks[PoseLandmarkType.leftShoulder]!;
 
-      double rangle = calculate(rightHip, rightShoulder, rightElbow);
+      double rangle = calculateAngle(rightHip.x, rightHip.y, rightShoulder.x,
+          rightShoulder.y, rightElbow.x, rightElbow.y);
 
-      double langle = calculate(leftHip, leftShoulder, leftElbow);
+      double langle = calculateAngle(leftHip.x, leftHip.y, leftShoulder.x,
+          leftShoulder.y, leftElbow.x, leftElbow.y);
 
       // Define a threshold angle to determine significant upward movement
       const double upwardMovementThreshold = 20.0;
@@ -86,6 +84,17 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
         leftControl();
         // Perform actions for the left arm's upward movement
       }
+
+      void clearPoses() {
+        // Clear poses logic goes here...
+        poses = [];
+      }
+
+      // Clear poses after a certain time
+      const clearDuration = Duration(seconds: 5);
+      Timer(clearDuration, () {
+        clearPoses();
+      });
 
       if (inputImage.inputImageData?.size != null &&
           inputImage.inputImageData?.imageRotation != null) {
@@ -110,41 +119,11 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
     }
   }
 
-  calculate(PoseLandmark hipLandmark, PoseLandmark shoulderLandmark,
-      PoseLandmark elbowLandmark) {
-    // Get the coordinates of the landmarks
-    final Offset hip = Offset(hipLandmark.x, hipLandmark.y);
-    final Offset shoulder = Offset(shoulderLandmark.x, shoulderLandmark.y);
-    final Offset elbow = Offset(elbowLandmark.x, elbowLandmark.y);
-
-    // Calculate the vectors from the hip to the shoulder and from the shoulder to the elbow
-    final Vector2 shoulderVector =
-        Vector2(shoulder.dx - hip.dx, shoulder.dy - hip.dy);
-    final Vector2 elbowVector =
-        Vector2(elbow.dx - shoulder.dx, elbow.dy - shoulder.dy);
-
-    // Calculate the dot product of the two vectors
-    final double dotProduct = shoulderVector.dot(elbowVector);
-
-    // Calculate the magnitudes of the vectors
-    final double shoulderMagnitude = shoulderVector.length;
-    final double elbowMagnitude = elbowVector.length;
-
-    // Calculate the angle between the vectors using the dot product and magnitudes
-    final double angle =
-        acos(dotProduct / (shoulderMagnitude * elbowMagnitude));
-
-    // Convert the angle to degrees
-    final double angleDegrees = angle * (180 / pi);
-
-    return angleDegrees;
-  }
-}
-  /*double calculateAngle(
+  double calculateAngle(
       double x1, double y1, double x2, double y2, double x3, double y3) {
     double angle = atan2(y1 - y2, x1 - x2) - atan2(y3 - y2, x3 - x2);
     angle = angle * 180 / pi;
     if (angle < 0) angle += 360;
     return angle;
-  }*/
-  
+  }
+}
